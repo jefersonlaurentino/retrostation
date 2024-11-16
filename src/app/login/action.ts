f05e2a39c3220cd8aad9d0485afb99e5805695d8
Jeferson Laurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2024-11-16 12:39:58 -0300

    created private routes

--- src/app/login/action.ts
+++ src/app/login/action.ts
@@ -1,35 +1,10 @@
-// import { NextResponse } from "next/server";
-
-// export function cookiesLogin() {
-//     const cookiesDate = NextResponse.next();
-//     cookiesDate.cookies.set('authToken', '')
-// }
-
-// pages/api/login.ts
-
-// import { NextResponse } from "next/server";
-// import { NextApiRequest, NextApiResponse } from "next";
-
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//     // Simula um login bem-sucedido
-//     const { username, password } = req.body;
-
-//     if (username === "admin" && password === "1234") {
-//         // Define o cookie authToken
-//         res.setHeader("Set-Cookie", `authToken=meu-token-seguro; Path=/; HttpOnly; Secure; SameSite=Strict`);
-//         return res.status(200).json({ message: "Login bem-sucedido!" });
-//     }
-
-//     return res.status(401).json({ message: "Credenciais inválidas!" });
-// }
-
-
+// criação da autenticação para as rotas privadas
 export default function handler() {
-    // Simula um login bem-sucedido
-    const authToken = "meu-token-seguro"; // O token gerado pelo servidor
+    const authToken = "meu-token-seguro";
     document.cookie = `authToken=${authToken}; path=/; Secure; SameSite=Strict`;
 }
 
+// remoção da autenticação
 export function logout() {
     document.cookie = "authToken=; path=/; Max-Age=0";
 }
